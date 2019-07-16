529e6521af36507a1a99ded8cc130aea54c695bc
Cesinha <cesinha51@gmail.com>
2019-07-16 11:41:03 -0300

    feat: add google images search

--- robots/image.js
+++ robots/image.js
@@ -8,16 +8,34 @@ const googleSearchCredentials = require('../credentials/google-search.json')
 async function robot() {
     const content = state.load()
 
-    const response = await customSearch.cse.list({
-        key: googleSearchCredentials.apikey,
-        cx: googleSearchCredentials.searchEngineId,
-        q: 'Michael Jackson',
-        searchType: 'image',
-        num: 2
-    })
+    await fetchImagesOfAllSentences(content)
 
-    console.dir(response, { depth: null })
-    process.exit(0)
+    state.save(content)
+    
+    async function fetchImagesOfAllSentences(content) {
+        for (const sentence of content.sentences) {
+            const query = `${content.searchTerm} ${sentence.keywords[0]}`
+            sentence.images = await fetchGoogleAndreturnImagesLinks(query)
+
+            sentence.googleSearchQuery = query
+        }
+    }
+    
+    async function fetchGoogleAndreturnImagesLinks(query) {
+        const response = await customSearch.cse.list({
+            key: googleSearchCredentials.apikey,
+            cx: googleSearchCredentials.searchEngineId,
+            q: query,
+            searchType: 'image',
+            num: 2
+        })
+
+        const imagesUrl =  response.data.items.map((item) => {
+            return item.link 
+        })
+
+        return imagesUrl
+    }
 }
 
 module.exports = robot
